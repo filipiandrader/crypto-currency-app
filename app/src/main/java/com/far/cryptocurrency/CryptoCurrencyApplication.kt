91579171e219713e5512fa44e47adb3fff7faf14
Filipi Andrade Rocha <filipiandrader@gmail.com>
2022-01-03 22:07:51 -0300

    Setting up the DI

---
+++ app/src/main/java/com/far/cryptocurrency/CryptoCurrencyApplication.kt
@@ -0,0 +1,7 @@
+package com.far.cryptocurrency
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CryptoCurrencyApplication : Application()
---
+++ app/src/main/java/com/far/cryptocurrency/di/AppModule.kt
@@ -0,0 +1,34 @@
+package com.far.cryptocurrency.di
+
+import com.far.cryptocurrency.common.Constants
+import com.far.cryptocurrency.data.remote.CoinPaprikaApi
+import com.far.cryptocurrency.data.repository.CoinRepositoryImpl
+import com.far.cryptocurrency.domain.repository.CoinRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun providePaprikaApi(): CoinPaprikaApi {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CoinPaprikaApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCoinRepository(api: CoinPaprikaApi): CoinRepository {
+        return CoinRepositoryImpl(api)
+    }
+}
