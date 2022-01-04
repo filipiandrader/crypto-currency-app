c6314a792ed1c24aa8af9d7756a6956a6529330e
Filipi Andrade Rocha <filipiandrader@gmail.com>
2022-01-03 21:10:48 -0300

    Setup app theme

--- app/src/main/java/com/far/cryptocurrency/MainActivity.kt
+++ app/src/main/java/com/far/cryptocurrency/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.far.cryptocurrency
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import com.far.cryptocurrency.theme.CryptocurrencyAppTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            CryptocurrencyAppTheme {
+                Surface(color = MaterialTheme.colors.background) {
+
+                }
+            }
+        }
     }
 }
